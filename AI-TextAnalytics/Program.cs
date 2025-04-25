fe7b3f7cd4e45f8ed228f0bac8c3a804ff04b031
RachitPandeyNeudesic <135119630+RachitPandeyNeudesic@users.noreply.github.com>
2025-04-25 19:46:35 +0530

    Added base project.

---
+++ AI-TextAnalytics/Program.cs
@@ -0,0 +1,31 @@
+﻿using AI_TextAnalytics;
+
+class Program
+{
+    public static string inputText =
+        "Unlike most of our gadgets, life doesn’t come with a How-To guide, or a Fix-It manual to download. But we can find solace in the wisdom and experiences of those who walked before us, such as Aristotle and Socrates.";
+    //"I had a lovely day today, the breeze was so soft and calm.";
+
+    // This is the main entry point for the application.
+    static void Main(string[] args)
+    {
+
+        // Print the input text to analyze.
+        Console.WriteLine("Input to analyze: {0}", inputText);
+
+        // Create an instance of the TextAnalytics class.
+        TextAnalytics textAnalytics = new TextAnalytics();
+
+        // Call the SentimentAnalysis method and print the result.
+        string sentimentResult = textAnalytics.SentimentAnalysis(inputText);
+
+        // Print the sentiment analysis result.
+        Console.WriteLine("\n\n\nOverall Sentiment: {0}", sentimentResult);
+
+        // Call the KeyPhraseExtraction method and print the result.
+        string keyPhrase = textAnalytics.KeyPhraseExtraction(inputText);
+
+        // Print the key phrase extraction result.
+        Console.WriteLine("\nKeyphrases: {0}", keyPhrase);
+    }
+}
---
+++ AI-TextAnalytics/TextAnalytics.cs
@@ -0,0 +1,92 @@
+﻿using Azure;
+using Azure.AI.TextAnalytics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI_TextAnalytics
+{
+    public class TextAnalytics
+    {
+        #region [Constructor]
+        
+        public TextAnalytics()
+        {
+            this.textAnalyticsClient = new TextAnalyticsClient(uri, azureKeyCredential);
+        }
+
+        #endregion
+
+        #region [Public Properties]
+
+        public AzureKeyCredential AzureKeyCredential
+        {
+            get
+            {
+                return azureKeyCredential;
+            }
+        }
+
+        public Uri Uri 
+        {
+            get
+            {
+                return uri;
+            }
+        }
+        
+        public TextAnalyticsClient TextAnalyticsClient 
+        {
+            get
+            {
+                return textAnalyticsClient;
+            }
+        }
+
+        #endregion
+
+        #region [Private Fields]
+
+        private AzureKeyCredential azureKeyCredential = new AzureKeyCredential("EKF7OyYzDDAh0aEUNk7byQRtoEukdzX3qy2GbJ22s4MYW9FGTrE2JQQJ99BDACYeBjFXJ3w3AAAaACOGmVut");
+        private Uri uri = new Uri("https://ai-102-text-analytics.cognitiveservices.azure.com/");
+        private TextAnalyticsClient textAnalyticsClient;
+
+        #endregion
+
+        #region [Public Methods]
+        
+        public string SentimentAnalysis(string inputText)
+        {
+            DocumentSentiment documentSentiment = textAnalyticsClient.AnalyzeSentiment(inputText);
+            ////documentSentiment.Sentences.ToList().ForEach(sentence =>
+            ////{
+            ////    Console.WriteLine($"Text: {sentence.Text}");
+            ////    Console.WriteLine($"Sentiment: {sentence.Sentiment}");
+            ////    Console.WriteLine($"Positive Score: {sentence.ConfidenceScores.Positive}");
+            ////    Console.WriteLine($"Neutral Score: {sentence.ConfidenceScores.Neutral}");
+            ////    Console.WriteLine($"Negative Score: {sentence.ConfidenceScores.Negative}");
+            ////});
+            StringBuilder overallSentiment = new StringBuilder();
+            overallSentiment.AppendLine($"Overall Sentiment: {documentSentiment.Sentiment}");
+            overallSentiment.AppendLine($"Positive Score: {documentSentiment.ConfidenceScores.Positive}");
+            overallSentiment.AppendLine($"Neutral Score: {documentSentiment.ConfidenceScores.Neutral}");
+            overallSentiment.AppendLine($"Negative Score: {documentSentiment.ConfidenceScores.Negative}");
+            
+            return overallSentiment.ToString();
+        }
+
+        public string KeyPhraseExtraction(string inputText)
+        {
+            KeyPhraseCollection keyPhrases = textAnalyticsClient.ExtractKeyPhrases(inputText);
+            ////foreach (string keyPhrase in keyPhrases)
+            ////{
+            ////    Console.WriteLine($"Key Phrase: {keyPhrase}");
+            ////}
+            return string.Join(", ", keyPhrases);
+        }
+
+        #endregion
+    }
+}
